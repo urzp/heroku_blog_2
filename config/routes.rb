04153d7f5de56c8e30588eb26bba708d5f6103ff
urzp <urzp002@mail.ru>
2016-04-29 20:46:06 +0300

    new fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-    #root to: 'articles#index'
-    root 'drinks#index'
+    root to: 'articles#index'
+    
     
     resources :articles do
 		resources :comments
